d4d1c7acf8087772b1847dfdf6b6372b0da8daa8
MelnikovDMA <starwarriordm@gmail.com>
2023-12-07 11:23:24 +0300

    gg

--- journal/app/app.py
+++ journal/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import sqlalchemy as sa
 import os
 
 app = Flask(__name__)
@@ -31,8 +32,48 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(app, metadata=metadata)
 migrate = Migrate(app, db)
 
-from models import Student, PlanOfStudy, Gradebook
+# from models import Student, PlanOfStudy, Gradebook
+
+class Student(db.Model):
+    __tablename__ = 'students'
+
+    id = db.Column(db.Integer, primary_key=True)
+    lastname = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=False)
+    admission_year = db.Column(db.Integer, nullable=False)
+    education_form = db.Column(db.String(100), nullable=False)
+    group = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<Student %r>' % self.lastname
     
+    # __table_args__ = {'extend_existing': True}
+    
+class PlanOfStudy(db.Model):
+    __tablename__ = 'planofstudy'
+
+    id = db.Column(db.Integer, primary_key=True)
+    speciality = db.Column(db.String(100), nullable=False)
+    discipline = db.Column(db.String(100), nullable=False)
+    semester = db.Column(db.Integer, nullable=False)
+    hours = db.Column(db.Integer, nullable=False)
+    exam_or_test = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return self.discipline
+
+class Gradebook(db.Model):
+    __tablename__ = 'gradebook'
+
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.Integer, nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey("students.id"), nullable=False)
+    discipline_id = db.Column(db.Integer, db.ForeignKey("planofstudy.id"), nullable=False)
+    mark = db.Column(db.Integer, nullable=False)
+
+    student = db.relationship('Student')
+    planofstudy = db.relationship('PlanOfStudy')   
 
 @app.route('/')
 def index():
@@ -50,7 +91,8 @@ def schetchik():
 @app.route('/disciplineinfo', methods=['GET', 'POST'])
 def disciplineinfo():
     if request.method == 'POST':
-        selected_discipline = request.form.get('discipline_name')
+        selected_discipline = request.form.get('discipline_name') or request.args.get('selected_discipline')
+        print(request.args)
         try:
             hours = PlanOfStudy.query.filter_by(discipline=selected_discipline).first().hours
             otchet = PlanOfStudy.query.filter_by(discipline=selected_discipline).first().exam_or_test
@@ -135,15 +177,14 @@ def createeducationplan():
 
 @app.route('/editeducationplan/<int:id>', methods=['GET', 'POST'])
 def editeducationplan(id):
+    educationplan = PlanOfStudy.query.get_or_404(id)
     if request.method == 'POST':
-        speciality = request.form.get('speciality')
-        discipline = request.form.get('discipline')
-        semester = request.form.get('semester')
-        hours = request.form.get('hours')
-        exam_or_test = request.form.get('exam_or_test')
+        educationplan.speciality = request.form.get('speciality')
+        educationplan.discipline = request.form.get('discipline')
+        educationplan.semester = request.form.get('semester')
+        educationplan.hours = request.form.get('hours')
+        educationplan.exam_or_test = request.form.get('exam_or_test')
         try:
-            plan = PlanOfStudy(speciality=speciality, discipline=discipline, semester=semester, hours=hours, exam_or_test=exam_or_test)
-            db.session.add(plan)
             db.session.commit()
             flash("Учебный план успешно отредактирован", "success")
             return redirect(url_for("educationplanslist"))
@@ -151,6 +192,57 @@ def editeducationplan(id):
             flash("Произошла ошибка", "danger")
             return render_template('editeducationplan/<int:id>.html', title='Редактирование учебного плана')
     
-    educationplan = PlanOfStudy.query.get(id)
     return render_template('editeducationplan.html', title='Редактирование учебного плана', educationplan=educationplan)
 
+@app.route('/gradelist')
+def gradelist():
+    students_arr = Student.query.all()
+    educationplans_arr = PlanOfStudy.query.all()
+    grade_arr = Gradebook.query.all()
+    
+    return render_template('gradelist.html', title='Успеваемость', grades=grade_arr, students=students_arr, educationplans = educationplans_arr)
+
+@app.route('/createmark', methods=['GET', 'POST'])
+def createmark():
+    if request.method == 'POST':
+        student_id = request.form['select_student']
+        discipline_id = request.form['select_discipline']
+        year = request.form.get('year')
+        mark = request.form.get('mark')
+        try:
+            grade = Gradebook(student_id=student_id, discipline_id=discipline_id, year=year, mark=mark)
+            db.session.add(grade)
+            db.session.commit()
+            flash("Оценка успешно добавлена", "success")
+            return redirect(url_for("gradelist"))
+        except:
+            flash("Произошла ошибка", "danger")
+            return render_template('createmark.html', title='Добавление оценки')
+    
+    students_arr = Student.query.all()
+    educationplans_arr = PlanOfStudy.query.all()
+    return render_template('createmark.html', title='Добавление оценки', students=students_arr, educationplans = educationplans_arr)
+
+@app.route('/editmark/<int:id>', methods=['GET', 'POST'])
+def editmark(id):
+    grade = Gradebook.query.get_or_404(id)
+    if request.method == 'POST':
+        grade.student_id = request.form['select_student']
+        grade.discipline_id = request.form['select_discipline']
+        grade.year = request.form.get('year')
+        grade.mark = request.form.get('mark')
+        try:
+            db.session.commit()
+            flash("Оценка успешно отредактирован", "success")
+            return redirect(url_for("gradelist"))
+        except:
+            flash("Произошла ошибка", "danger")
+            return render_template('editmark/<int:id>.html', title='Редактирование оценки')
+    
+    students_arr = Student.query.all()
+    educationplans_arr = PlanOfStudy.query.all()
+
+    return render_template('editmark.html', title='Редактирование оценки', grade=grade, students=students_arr, educationplans = educationplans_arr)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
